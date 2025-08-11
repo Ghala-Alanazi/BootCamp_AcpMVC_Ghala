a1e2ea4e228347a6f13bd07f6daeeacadb829d3d
salh0 <salh0@ZVXNL>
2025-08-11 23:13:13 +0300

    Add project files.

---
+++ BootCamp_AcpMVC_Ghala/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BootCamp_AcpMVC_Ghala.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BootCamp_AcpMVC_Ghala.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option):base(option) 
+        { 
+        
+        }
+            public DbSet<Category> Categories { get; set; }
+
+        }
+    }
+
---
+++ BootCamp_AcpMVC_Ghala/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BootCamp_AcpMVC_Ghala.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
